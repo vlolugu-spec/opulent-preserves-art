ef6b4fa35d90174170aa100040bf98d9c403d1f0
vlolugu-spec <vlolugu@gitam.in>
2025-10-31 23:24:25 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,19 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/opulent-preserves-art/" : "/",
+
   server: {
     host: "::",
     port: 8080,
   },
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 }));
+
